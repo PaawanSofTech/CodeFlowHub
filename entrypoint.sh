7a9734929eb9631555fe87adb5c651da70c1b9f7
PaawanSofTech <paawansharma0304@gmail.com>
2026-04-09 02:24:34 +0530

    entrypoint fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-# ASP.NET 8 reads HTTP_PORTS automatically when set by Railway.
-# We only set ASPNETCORE_URLS if HTTP_PORTS is absent (local Docker, etc.)
-if [ -z "$HTTP_PORTS" ] && [ -n "$PORT" ]; then
-  export ASPNETCORE_URLS="http://+:${PORT}"
-fi
 exec dotnet CodeFlow.API.dll
